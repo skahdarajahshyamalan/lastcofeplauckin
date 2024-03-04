82c771a2f4c5fb6cbdbdbb923c2169f184ff4d51
RoyceRocithan <sivathanr5@gmail.com>
2024-03-04 15:50:54 +0530

    add_dubg

--- activate-plugin.php
+++ activate-plugin.php
@@ -1,11 +1,12 @@
 <?php
 
-function my_custom_plugin_install() {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'cofebook';
-    $table_name2 = $wpdb->prefix . 'cofebookconfig';
-    $charset_collate = $wpdb->get_charset_collate();
-    $sql = "CREATE TABLE $table_name (
+function my_custom_plugin_install()
+{
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'cofebook';
+  $table_name2 = $wpdb->prefix . 'cofebookconfig';
+  $charset_collate = $wpdb->get_charset_collate();
+  $sql = "CREATE TABLE $table_name (
         id mediumint(9) NOT NULL AUTO_INCREMENT,
         fullname varchar(255) NOT NULL,
         email varchar(255) NOT NULL,
@@ -17,13 +18,28 @@ function my_custom_plugin_install() {
         outdor varchar(255) NOT NULL,
         PRIMARY KEY  (id)
     ) $charset_collate;";
-    $sql2 = "CREATE TABLE $table_name2 (
+  $sql2 = "CREATE TABLE $table_name2 (
       id mediumint(9) NOT NULL AUTO_INCREMENT,
       config varchar(255) NOT NULL,
       PRIMARY KEY  (id)
   ) $charset_collate;";
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql);
-    dbDelta($sql2);
+  require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+  dbDelta($sql);
+  dbDelta($sql2);
+}
+function insert_data_into_table()
+{
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'cofebookconfig';
+
+  $data = array(
+    'id' => '1',
+    'config' => '["09:00","10:00","11:00","12:00","13:00","14:00"]',
+  );
+
+  $result = $wpdb->insert($table_name, $data);
+
+  if (is_wp_error($result)) {
+    echo 'Error inserting data: ' . $result->get_error_message();
   }
-?>
+}
--- cofe.php
+++ cofe.php
@@ -107,7 +107,7 @@ EOT;
   return $markup;
 }
 
-add_shortcode('cofe_code', 'example_form_plugin');
+add_shortcode('cofe_codedev', 'example_form_plugin');
 add_action('wp_head', 'contact_form_js');
 function contact_form_process()
 {
@@ -206,6 +206,7 @@ function enqueue_custom_scripts_and_styles()
   ));
   wp_enqueue_script('bootstrapjs', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js', array(), '3.6.4', true);
   wp_enqueue_style('bootstrap-style', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css', array(), '2.5.20');
+  // wp_enqueue_script('datatables', 'https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js', array('jquery'), '1.10.24', true);
 }
 
 // Enqueue Bootstrap CSS
@@ -221,13 +222,25 @@ function enqueue_bootstrap_js()
   wp_enqueue_script('bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array('jquery'), '4.5.2', true);
 }
 //function datatable
+function enqueue_bootstrapdatatable_js()
+{
+  wp_enqueue_script('bootstrapdatatable-js', 'https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js', array('jquery'), '4.5.2', true);
+}
+function enqueue_datatable_css()
+{
+  wp_enqueue_style('datatable_css', 'https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css');
+}
+add_action('admin_enqueue_scripts', 'enqueue_bootstrap_css');
+add_action('admin_enqueue_scripts', 'enqueue_datatable_css');
+//https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css
 
-
-
-
+add_action('admin_enqueue_scripts', 'enqueue_bootstrapdatatable_js');
 add_action('admin_enqueue_scripts', 'enqueue_bootstrap_js');
 add_action('wp_enqueue_scripts', 'enqueue_custom_scripts_and_styles');
 add_action('init', 'contact_form_process');
+add_action('init', 'insert_data_into_table');
+
+
 
 
 
@@ -258,492 +271,41 @@ function my_plugin_settings_page()
 ?>
   <div class="container">
     <div class="mt-5 mr-10">
-      <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <form method="post" action="<?php echo esc_url( admin_url('admin-post.php') ); ?>">
+        <div class="mb-3 ">
+          <input type="text" name="column1" class="form-controll" id="exampleCheck1"> format ["09:00","10:00","11:00","12:00","13:00","14:00"]
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+
+        <button type="submit"  class="btn btn-primary">Submit</button>
       </form>
     </div>
-    <p>result</p>
+    <p>List all the member</p>
     <table id="example" class="display" style="width:100%">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Position</th>
-          <th>Office</th>
-          <th>Age</th>
-          <th>Start date</th>
-          <th>Salary</th>
+          <th>FullName</th>
+          <th>email</th>
+          <th>phone</th>
+          <th>date</th>
+          <th>guests</th>
+          <th>des</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Tiger Nixon</td>
-          <td>System Architect</td>
-          <td>Edinburgh</td>
-          <td>61</td>
-          <td>2011-04-25</td>
-          <td>$320,800</td>
-        </tr>
-        <tr>
-          <td>Garrett Winters</td>
-          <td>Accountant</td>
-          <td>Tokyo</td>
-          <td>63</td>
-          <td>2011-07-25</td>
-          <td>$170,750</td>
-        </tr>
-        <tr>
-          <td>Ashton Cox</td>
-          <td>Junior Technical Author</td>
-          <td>San Francisco</td>
-          <td>66</td>
-          <td>2009-01-12</td>
-          <td>$86,000</td>
-        </tr>
-        <tr>
-          <td>Cedric Kelly</td>
-          <td>Senior Javascript Developer</td>
-          <td>Edinburgh</td>
-          <td>22</td>
-          <td>2012-03-29</td>
-          <td>$433,060</td>
-        </tr>
-        <tr>
-          <td>Airi Satou</td>
-          <td>Accountant</td>
-          <td>Tokyo</td>
-          <td>33</td>
-          <td>2008-11-28</td>
-          <td>$162,700</td>
-        </tr>
-        <tr>
-          <td>Brielle Williamson</td>
-          <td>Integration Specialist</td>
-          <td>New York</td>
-          <td>61</td>
-          <td>2012-12-02</td>
-          <td>$372,000</td>
-        </tr>
-        <tr>
-          <td>Herrod Chandler</td>
-          <td>Sales Assistant</td>
-          <td>San Francisco</td>
-          <td>59</td>
-          <td>2012-08-06</td>
-          <td>$137,500</td>
-        </tr>
-        <tr>
-          <td>Rhona Davidson</td>
-          <td>Integration Specialist</td>
-          <td>Tokyo</td>
-          <td>55</td>
-          <td>2010-10-14</td>
-          <td>$327,900</td>
-        </tr>
-        <tr>
-          <td>Colleen Hurst</td>
-          <td>Javascript Developer</td>
-          <td>San Francisco</td>
-          <td>39</td>
-          <td>2009-09-15</td>
-          <td>$205,500</td>
-        </tr>
-        <tr>
-          <td>Sonya Frost</td>
-          <td>Software Engineer</td>
-          <td>Edinburgh</td>
-          <td>23</td>
-          <td>2008-12-13</td>
-          <td>$103,600</td>
-        </tr>
-        <tr>
-          <td>Jena Gaines</td>
-          <td>Office Manager</td>
-          <td>London</td>
-          <td>30</td>
-          <td>2008-12-19</td>
-          <td>$90,560</td>
-        </tr>
-        <tr>
-          <td>Quinn Flynn</td>
-          <td>Support Lead</td>
-          <td>Edinburgh</td>
-          <td>22</td>
-          <td>2013-03-03</td>
-          <td>$342,000</td>
-        </tr>
-        <tr>
-          <td>Charde Marshall</td>
-          <td>Regional Director</td>
-          <td>San Francisco</td>
-          <td>36</td>
-          <td>2008-10-16</td>
-          <td>$470,600</td>
-        </tr>
-        <tr>
-          <td>Haley Kennedy</td>
-          <td>Senior Marketing Designer</td>
-          <td>London</td>
-          <td>43</td>
-          <td>2012-12-18</td>
-          <td>$313,500</td>
-        </tr>
-        <tr>
-          <td>Tatyana Fitzpatrick</td>
-          <td>Regional Director</td>
-          <td>London</td>
-          <td>19</td>
-          <td>2010-03-17</td>
-          <td>$385,750</td>
-        </tr>
-        <tr>
-          <td>Michael Silva</td>
-          <td>Marketing Designer</td>
-          <td>London</td>
-          <td>66</td>
-          <td>2012-11-27</td>
-          <td>$198,500</td>
-        </tr>
-        <tr>
-          <td>Paul Byrd</td>
-          <td>Chief Financial Officer (CFO)</td>
-          <td>New York</td>
-          <td>64</td>
-          <td>2010-06-09</td>
-          <td>$725,000</td>
-        </tr>
-        <tr>
-          <td>Gloria Little</td>
-          <td>Systems Administrator</td>
-          <td>New York</td>
-          <td>59</td>
-          <td>2009-04-10</td>
-          <td>$237,500</td>
-        </tr>
-        <tr>
-          <td>Bradley Greer</td>
-          <td>Software Engineer</td>
-          <td>London</td>
-          <td>41</td>
-          <td>2012-10-13</td>
-          <td>$132,000</td>
-        </tr>
-        <tr>
-          <td>Dai Rios</td>
-          <td>Personnel Lead</td>
-          <td>Edinburgh</td>
-          <td>35</td>
-          <td>2012-09-26</td>
-          <td>$217,500</td>
-        </tr>
-        <tr>
-          <td>Jenette Caldwell</td>
-          <td>Development Lead</td>
-          <td>New York</td>
-          <td>30</td>
-          <td>2011-09-03</td>
-          <td>$345,000</td>
-        </tr>
-        <tr>
-          <td>Yuri Berry</td>
-          <td>Chief Marketing Officer (CMO)</td>
-          <td>New York</td>
-          <td>40</td>
-          <td>2009-06-25</td>
-          <td>$675,000</td>
-        </tr>
-        <tr>
-          <td>Caesar Vance</td>
-          <td>Pre-Sales Support</td>
-          <td>New York</td>
-          <td>21</td>
-          <td>2011-12-12</td>
-          <td>$106,450</td>
-        </tr>
-        <tr>
-          <td>Doris Wilder</td>
-          <td>Sales Assistant</td>
-          <td>Sydney</td>
-          <td>23</td>
-          <td>2010-09-20</td>
-          <td>$85,600</td>
-        </tr>
-        <tr>
-          <td>Angelica Ramos</td>
-          <td>Chief Executive Officer (CEO)</td>
-          <td>London</td>
-          <td>47</td>
-          <td>2009-10-09</td>
-          <td>$1,200,000</td>
-        </tr>
-        <tr>
-          <td>Gavin Joyce</td>
-          <td>Developer</td>
-          <td>Edinburgh</td>
-          <td>42</td>
-          <td>2010-12-22</td>
-          <td>$92,575</td>
-        </tr>
-        <tr>
-          <td>Jennifer Chang</td>
-          <td>Regional Director</td>
-          <td>Singapore</td>
-          <td>28</td>
-          <td>2010-11-14</td>
-          <td>$357,650</td>
-        </tr>
-        <tr>
-          <td>Brenden Wagner</td>
-          <td>Software Engineer</td>
-          <td>San Francisco</td>
-          <td>28</td>
-          <td>2011-06-07</td>
-          <td>$206,850</td>
-        </tr>
-        <tr>
-          <td>Fiona Green</td>
-          <td>Chief Operating Officer (COO)</td>
-          <td>San Francisco</td>
-          <td>48</td>
-          <td>2010-03-11</td>
-          <td>$850,000</td>
-        </tr>
-        <tr>
-          <td>Shou Itou</td>
-          <td>Regional Marketing</td>
-          <td>Tokyo</td>
-          <td>20</td>
-          <td>2011-08-14</td>
-          <td>$163,000</td>
-        </tr>
-        <tr>
-          <td>Michelle House</td>
-          <td>Integration Specialist</td>
-          <td>Sydney</td>
-          <td>37</td>
-          <td>2011-06-02</td>
-          <td>$95,400</td>
-        </tr>
-        <tr>
-          <td>Suki Burks</td>
-          <td>Developer</td>
-          <td>London</td>
-          <td>53</td>
-          <td>2009-10-22</td>
-          <td>$114,500</td>
-        </tr>
-        <tr>
-          <td>Prescott Bartlett</td>
-          <td>Technical Author</td>
-          <td>London</td>
-          <td>27</td>
-          <td>2011-05-07</td>
-          <td>$145,000</td>
-        </tr>
-        <tr>
-          <td>Gavin Cortez</td>
-          <td>Team Leader</td>
-          <td>San Francisco</td>
-          <td>22</td>
-          <td>2008-10-26</td>
-          <td>$235,500</td>
-        </tr>
-        <tr>
-          <td>Martena Mccray</td>
-          <td>Post-Sales support</td>
-          <td>Edinburgh</td>
-          <td>46</td>
-          <td>2011-03-09</td>
-          <td>$324,050</td>
-        </tr>
-        <tr>
-          <td>Unity Butler</td>
-          <td>Marketing Designer</td>
-          <td>San Francisco</td>
-          <td>47</td>
-          <td>2009-12-09</td>
-          <td>$85,675</td>
-        </tr>
-        <tr>
-          <td>Howard Hatfield</td>
-          <td>Office Manager</td>
-          <td>San Francisco</td>
-          <td>51</td>
-          <td>2008-12-16</td>
-          <td>$164,500</td>
-        </tr>
-        <tr>
-          <td>Hope Fuentes</td>
-          <td>Secretary</td>
-          <td>San Francisco</td>
-          <td>41</td>
-          <td>2010-02-12</td>
-          <td>$109,850</td>
-        </tr>
-        <tr>
-          <td>Vivian Harrell</td>
-          <td>Financial Controller</td>
-          <td>San Francisco</td>
-          <td>62</td>
-          <td>2009-02-14</td>
-          <td>$452,500</td>
-        </tr>
-        <tr>
-          <td>Timothy Mooney</td>
-          <td>Office Manager</td>
-          <td>London</td>
-          <td>37</td>
-          <td>2008-12-11</td>
-          <td>$136,200</td>
-        </tr>
-        <tr>
-          <td>Jackson Bradshaw</td>
-          <td>Director</td>
-          <td>New York</td>
-          <td>65</td>
-          <td>2008-09-26</td>
-          <td>$645,750</td>
-        </tr>
-        <tr>
-          <td>Olivia Liang</td>
-          <td>Support Engineer</td>
-          <td>Singapore</td>
-          <td>64</td>
-          <td>2011-02-03</td>
-          <td>$234,500</td>
-        </tr>
-        <tr>
-          <td>Bruno Nash</td>
-          <td>Software Engineer</td>
-          <td>London</td>
-          <td>38</td>
-          <td>2011-05-03</td>
-          <td>$163,500</td>
-        </tr>
-        <tr>
-          <td>Sakura Yamamoto</td>
-          <td>Support Engineer</td>
-          <td>Tokyo</td>
-          <td>37</td>
-          <td>2009-08-19</td>
-          <td>$139,575</td>
-        </tr>
-        <tr>
-          <td>Thor Walton</td>
-          <td>Developer</td>
-          <td>New York</td>
-          <td>61</td>
-          <td>2013-08-11</td>
-          <td>$98,540</td>
-        </tr>
-        <tr>
-          <td>Finn Camacho</td>
-          <td>Support Engineer</td>
-          <td>San Francisco</td>
-          <td>47</td>
-          <td>2009-07-07</td>
-          <td>$87,500</td>
-        </tr>
-        <tr>
-          <td>Serge Baldwin</td>
-          <td>Data Coordinator</td>
-          <td>Singapore</td>
-          <td>64</td>
-          <td>2012-04-09</td>
-          <td>$138,575</td>
-        </tr>
-        <tr>
-          <td>Zenaida Frank</td>
-          <td>Software Engineer</td>
-          <td>New York</td>
-          <td>63</td>
-          <td>2010-01-04</td>
-          <td>$125,250</td>
-        </tr>
-        <tr>
-          <td>Zorita Serrano</td>
-          <td>Software Engineer</td>
-          <td>San Francisco</td>
-          <td>56</td>
-          <td>2012-06-01</td>
-          <td>$115,000</td>
-        </tr>
-        <tr>
-          <td>Jennifer Acosta</td>
-          <td>Junior Javascript Developer</td>
-          <td>Edinburgh</td>
-          <td>43</td>
-          <td>2013-02-01</td>
-          <td>$75,650</td>
-        </tr>
-        <tr>
-          <td>Cara Stevens</td>
-          <td>Sales Assistant</td>
-          <td>New York</td>
-          <td>46</td>
-          <td>2011-12-06</td>
-          <td>$145,600</td>
-        </tr>
-        <tr>
-          <td>Hermione Butler</td>
-          <td>Regional Director</td>
-          <td>London</td>
-          <td>47</td>
-          <td>2011-03-21</td>
-          <td>$356,250</td>
-        </tr>
-        <tr>
-          <td>Lael Greer</td>
-          <td>Systems Administrator</td>
-          <td>London</td>
-          <td>21</td>
-          <td>2009-02-27</td>
-          <td>$103,500</td>
-        </tr>
-        <tr>
-          <td>Jonas Alexander</td>
-          <td>Developer</td>
-          <td>San Francisco</td>
-          <td>30</td>
-          <td>2010-07-14</td>
-          <td>$86,500</td>
-        </tr>
-        <tr>
-          <td>Shad Decker</td>
-          <td>Regional Director</td>
-          <td>Edinburgh</td>
-          <td>51</td>
-          <td>2008-11-13</td>
-          <td>$183,000</td>
-        </tr>
-        <tr>
-          <td>Michael Bruce</td>
-          <td>Javascript Developer</td>
-          <td>Singapore</td>
-          <td>29</td>
-          <td>2011-06-27</td>
-          <td>$183,000</td>
-        </tr>
-        <tr>
-          <td>Donna Snider</td>
-          <td>Customer Support</td>
-          <td>New York</td>
-          <td>27</td>
-          <td>2011-01-25</td>
-          <td>$112,000</td>
-        </tr>
+        <?php
+        global $wpdb;
+        $results = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}cofebook", ARRAY_A);
+        foreach ($results as $row) {
+          echo "<tr>";
+          echo "<td>{$row['fullname']}</td>";
+          echo "<td>{$row['email']}</td>";
+          echo "<td>{$row['phone']}</td>";
+          echo "<td>{$row['date_time']}</td>";
+          echo "<td>{$row['guests']}</td>";
+          echo "<td>{$row['descript']}</td>";
+          echo "</tr>";
+        }
+        ?>
       </tbody>
 
     </table>
@@ -773,21 +335,48 @@ function my_plugin_text_field_callback()
   $value = get_option('my_plugin_option');
   echo '<input type="text" name="my_plugin_option" value="' . esc_attr($value) . '" />';
 }
+
+add_action( 'admin_post_nopriv_update_table_data', 'update_table_data' );
+
+function update_table_data() {
+  global $wpdb;
+
+  
+  if ( isset( $_POST['column1'] ) ) {
+      $column1_value = sanitize_text_field( $_POST['column1'] );
+      
+      $table_name = $wpdb->prefix . 'cofebookconfig';
+      $data_to_update = array(
+          'config' => $column1_value,
+          );
+      $where = array( 'id' => 1);
+      $wpdb->update( $table_name, $data_to_update, $where );
+      wp_redirect( $_SERVER['HTTP_REFERER'] );
+      exit;
+  }
+}
 ?>
 <?php
- function contact_form_js() { ?>
- <script type="text/javascript">
-  var table = $('#example').DataTable();
-      function validateForm(form) {
-      
-        var errors = '';
-         
-        var regexpEmail = /\w{1,}[@][\w\-]{1,}([.]([\w\-]{1,})){1,3}$/;
-         if (!regexpEmail.test(form.Email.value)) errors += "Error :  your e-mail address format incorrect.\n";
-         if (form.Name.value.trim() === '') { errors += "Error: Full name cannot be empty.\n"; }
-         if (errors != '') { alert(errors); return false;  }
-           return true;     
+function contact_form_js()
+{ ?>
+  <script type="text/javascript">
+    var table = $('#example').DataTable();
+
+    function validateForm(form) {
+
+      var errors = '';
+
+      var regexpEmail = /\w{1,}[@][\w\-]{1,}([.]([\w\-]{1,})){1,3}$/;
+      if (!regexpEmail.test(form.Email.value)) errors += "Error :  your e-mail address format incorrect.\n";
+      if (form.Name.value.trim() === '') {
+        errors += "Error: Full name cannot be empty.\n";
       }
-      </script>
-       <?php }
-       ?>
+      if (errors != '') {
+        alert(errors);
+        return false;
+      }
+      return true;
+    }
+  </script>
+<?php }
+?>
